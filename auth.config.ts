98973393c2c9d736a2aa8eabfbdf9b8bb4934dbb
birdinterface <alexgisbrecht20@gmail.com>
2025-04-08 10:14:45 +0200

    v7

---
+++ auth.config.ts
@@ -0,0 +1,37 @@
+import { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+    newUser: "/",
+  },
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      let isLoggedIn = !!auth?.user;
+      let isOnChat = nextUrl.pathname.startsWith("/");
+      let isOnRegister = nextUrl.pathname.startsWith("/register");
+      let isOnLogin = nextUrl.pathname.startsWith("/login");
+
+      if (isOnChat && !isLoggedIn) {
+        return false;
+      }
+
+      if (isOnRegister && isLoggedIn) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (isOnLogin && isLoggedIn) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      return true;
+    },
+  },
+} satisfies NextAuthConfig; 
