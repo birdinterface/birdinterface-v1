588ccef6dc08a24395a6aa198d61201abf1dbcda
birdinterface <alexgisbrecht20@gmail.com>
2025-04-11 15:58:01 +0200

    9

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,6 +10,8 @@ import {
 import { clsx, type ClassValue } from "clsx";
 import { formatDistance, subDays, format, isWithinInterval, startOfMonth } from 'date-fns';
 import { twMerge } from "tailwind-merge";
+import { Chat } from '@/db/schema';
+import { fetcher } from '@/lib/utils';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -19,23 +21,6 @@ interface ApplicationError extends Error {
   info: string;
   status: number;
 }
-
-export const fetcher = async (url: string) => {
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    const error = new Error(
-      "An error occurred while fetching the data.",
-    ) as ApplicationError;
-
-    error.info = await res.json();
-    error.status = res.status;
-
-    throw error;
-  }
-
-  return res.json();
-};
 
 export function generateUUID(): string {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
