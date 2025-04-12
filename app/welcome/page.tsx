7685aae02b41cd35e53840f8567ccb45ccc654ce
birdinterface <alexgisbrecht20@gmail.com>
2025-04-12 22:10:43 +0200

    v3

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 import GrainyGradientGlow from '@/app/components/GrainyGradientGlow';
+
 import '../../public/css/normalize.css';
 import '../../public/css/webflow.css';
 import '../../public/css/advancers-club-ef3cf37311bfc4b53cc064fc.webflow.css';
