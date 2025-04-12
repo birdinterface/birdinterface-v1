21845cb21908df2588e2c4c2d8df5a440830a5bb
birdinterface <alexgisbrecht20@gmail.com>
2025-04-12 22:17:16 +0200

    v5

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -10,7 +10,6 @@ import '../../public/css/normalize.css';
 import '../../public/css/webflow.css';
 import '../../public/css/advancers-club-ef3cf37311bfc4b53cc064fc.webflow.css';
 import '../../public/css/stars.css';
-
 import { PreloadImages } from '@/components/PreloadImages';
 import { criticalImages } from '@/lib/images';
 
